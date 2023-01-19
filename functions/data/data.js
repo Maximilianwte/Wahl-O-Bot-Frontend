fd4038a78d8a9df35e94719214b559b1600f52bf
Maximilian Witte <maximilianwitte@outlook.de>
2023-01-19 22:25:54 +0100

    update firebase key

--- functions/data/data.js
+++ functions/data/data.js
@@ -7,17 +7,17 @@ const serverless = require("serverless-http");
 var admin = require("firebase-admin");
 
 var serviceAccount = {
-  type: "service_account",
-  project_id: "uhh-wahlcheck-ai",
-  private_key_id: process.env.VUE_APP_FIREBASE_PRIVATE_ID,
-  private_key: process.env.VUE_APP_FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n"),
-  client_email: "firebase-adminsdk-n9jjt@uhh-wahlcheck-ai.iam.gserviceaccount.com",
-  client_id: "109089163775563397101",
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-n9jjt%40uhh-wahlcheck-ai.iam.gserviceaccount.com"
-} 
+  "type": "service_account",
+  "project_id": "uhh-wahlcheck-ai",
+  "private_key_id": process.env.VUE_APP_FIREBASE_PRIVATE_ID,
+  "private_key": process.env.VUE_APP_FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n"),
+  "client_email": "firebase-adminsdk-n9jjt@uhh-wahlcheck-ai.iam.gserviceaccount.com",
+  "client_id": "109089163775563397101",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-n9jjt%40uhh-wahlcheck-ai.iam.gserviceaccount.com"
+}
 if (admin.apps.length === 0) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
